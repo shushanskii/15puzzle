d4ee23280fe4d17ca2b433d4606ddbd974c1d468
Konstantin Shushanskii <konstantin.shushanskii@hqrevenue.com>
2023-12-23 18:59:41 +0100

    feat(chore): add webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    })
+  ]
+}
